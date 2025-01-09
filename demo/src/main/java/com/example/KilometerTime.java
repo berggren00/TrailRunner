32c05478056aa08ae88336deec4b9952a8a80a31
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-01-09 15:04:03 +0100

    KilometerTime method start

--- demo/src/main/java/com/example/KilometerTime.java
+++ demo/src/main/java/com/example/KilometerTime.java
@@ -1,3 +1,27 @@
+package com.example;
+
+
 public class KilometerTime{
-    
+
+    RunTime runtime = new RunTime(0, 0, 0);
+    AddDistance addDistance = new AddDistance();
+
+    public static double calculateKilometerTime(Runtime runTime, AddDistance addDistance) {
+        if (runTime == null || addDistance == null) {
+            return -1;
+        }
+        int hours = runTime.getHours();
+        int minutes = runTime.getMinutes();
+        int seconds = runTime.getSeconds();
+
+        double totalMinutes = hours * 60 + minutes + seconds / 60.0;
+
+        double totalDistance = addDistance.getTotalDistance();
+
+        if (totalMinutes < 0 || totalDistance <= 0) {
+            return -1;
+        }
+        return totalMinutes/totalDistance;
+    }
+
 }
---
+++ demo/src/test/java/TestKilometerTime.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import com.example.KilometerTime;
+
+public class TestKilometerTime {
+    @Test
+    KilometerTime kmTime = new KilometerTime();
+    kmTime.calculateKilometerTime();
+    assertEquals();
+}
