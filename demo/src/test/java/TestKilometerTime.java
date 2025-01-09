5bcfb69eb696c3c22f1809140465631c6ee5c382
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-01-09 21:23:54 +0100

    More test on "TestKilometerTime"

--- demo/src/test/java/TestKilometerTime.java
+++ demo/src/test/java/TestKilometerTime.java
@@ -8,14 +8,54 @@ import com.example.RunTime;
 public class TestKilometerTime {
 
     @Test
-    public void testKillometerTime() {
-    RunTime runTime = new RunTime(1, 30, 0);
+    public void testValidKilometerTime(){
+        RunTime runTime = new RunTime(1, 30, 0);
+        AddDistance addDistance = new AddDistance();
+
+        addDistance.addDistance(15);
+
+        double result = KilometerTime.calculateKilometerTime(runTime, addDistance);
+        assertEquals(6.0, result, 0.01);
+
+    }
+
+    @Test
+    public void testZeroDistance(){
+        RunTime runTime = new RunTime(1, 0, 0);
+        AddDistance addDistance = new AddDistance();
+
+        double result = KilometerTime.calculateKilometerTime(runTime, addDistance);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void testZeroTime(){
+        RunTime runTime = new RunTime(0, 0, 0);
+        AddDistance addDistance = new AddDistance();
+        addDistance.addDistance(10);
+
+        double result = KilometerTime.calculateKilometerTime(runTime, addDistance);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testNegativeDistance() {
+        RunTime runTime = new RunTime(1, 0, 0);
+        AddDistance addDistance = new AddDistance();
+        addDistance.addDistance(-5);
+
+        double result = KilometerTime.calculateKilometerTime(runTime, addDistance);
+        assertEquals(-1, result);
+
+    }
+
+    @Test
+    public void testNegativeTime() {
+    RunTime runTime = new RunTime(-1, 0, 0);
     AddDistance addDistance = new AddDistance();
 
-    addDistance.addDistance(15.0);
-
     double result = KilometerTime.calculateKilometerTime(runTime, addDistance);
-    
-    assertEquals(6.0, result, 0.01);
+    assertEquals(-1, result);
     }
+
 }
