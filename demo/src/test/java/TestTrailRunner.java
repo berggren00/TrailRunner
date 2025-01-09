c95d6256b33d87f9d8bcf3b1acd9e2b75ec61ac6
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-01-09 11:05:28 +0100

    Change of import

--- demo/src/test/java/TestTrailRunner.java
+++ demo/src/test/java/TestTrailRunner.java
@@ -1,6 +1,5 @@
-import org.junit.jupter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 
 public class TrailRunner {
